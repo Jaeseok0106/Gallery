46b2bfe641c94f59a5916640834f7651daa40c66
sdo06@naver.com <sdo06@naver.com>
2022-07-27 09:13:44 +0900

    20220727 - kakao Login

--- src/main/java/com/human/gallery/domain/user/Users.java
+++ src/main/java/com/human/gallery/domain/user/Users.java
@@ -1,6 +1,10 @@
 package com.human.gallery.domain.user;
 
 import lombok.Data;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.security.core.userdetails.User;
+
+import javax.sql.DataSource;
 
 @Data
 public class Users {
@@ -15,5 +19,4 @@ public class Users {
 	private String username;
 	private int postcode;
 	private String mobile;
-
 }
